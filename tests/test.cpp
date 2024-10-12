560b6670cbb2575ca291a4b3454140237a8502a8
Vyron Avramidis <vyronavramidis@gmail.com>
2024-10-12 10:16:57 +0100

    Adds tests in pipeline

---
+++ tests/test.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include <WordCount.hpp>
+
+TEST(CPPWC_Test, GetBytesTest) {
+    auto count = std::make_unique<WordCount>("../tests/test.txt");
+    auto result = count->GetBytes();
+    EXPECT_EQ(result, 342190);
+}
+
+TEST(CPPWC_Test, GetLineTest) {
+    auto count = std::make_unique<WordCount>("../tests/test.txt");
+    auto result = count->GetLines();
+    EXPECT_EQ(result, 7145);
+}
+
+TEST(CPPWC_Test, GetWordsTest) {
+    auto count = std::make_unique<WordCount>("../tests/test.txt");
+    auto result = count->GetWords();
+    EXPECT_EQ(result, 58164);
+}
+
+TEST(CPPWC_Test, GetCharsTest) {
+    auto count = std::make_unique<WordCount>("../tests/test.txt");
+    auto result = count->GetChars();
+    EXPECT_EQ(result, 339292);
+}
